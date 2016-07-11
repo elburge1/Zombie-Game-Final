4fd4752bea7badd7c82fffac84ffc546641976df
Edward Burgess <elburgess1@gmail.com>
2016-07-11 09:57:56 -0700

    final commit before freeze

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,17 @@ var scoresModel = require('../models/scores')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var user = res.locals.user;
-
-  
+  var user = res.locals.user
+  Promise.all([scoresModel.getAllScores()])
+  .then(data => {
+    var scores = data;
+  })
+  res.render('index');
 });
+
+router.get('/logout', function(req, res, next) {
+  req.session = null;
+  res.redirect('/');
+})
 
 module.exports = router;
