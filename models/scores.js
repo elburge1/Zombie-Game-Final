d30edc71424d37cdaae5e35edddc58e34535fd7c
Edward Burgess <elburgess1@gmail.com>
2016-07-11 00:21:54 -0700

    set up functions to grab scores

--- models/scores.js
+++ models/scores.js
@@ -6,5 +6,11 @@ module.exports = {
       user_id: user_id,
       score: score,
     });
+  },
+  getAllScores: function(){
+    return knex('user_scores');
+  },
+  getTopScores: function(){
+    return knex('user_scores').orderBy('score', 'desc');
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise');
+var
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
