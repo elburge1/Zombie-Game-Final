cd2317fb0da360d79c866870981d19e0d4730daa
Edward Burgess <elburgess1@gmail.com>
2016-07-26 13:07:38 -0700

    scoreboard in progress

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,14 @@ var usersModel = require('../models/users');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var user = res.locals.user;
-  Promise.all([scoresModel.getAllScores()])
-  .then(data => {
-    var scores = data;
-    return data;
+  Promise.all([scoresModel.getTopScores()])
+  .then(scoredata => {
+    var scores = scoredata[0];
+    res.locals.pageData = {};
+    res.locals.pageData.scores = [];
+    for (var j = 0; j < scoredata[0].length; j++){
+      res.locals.pageData.scores.push(scores[j].score)
+    }
   })
   res.render('index');
 });
